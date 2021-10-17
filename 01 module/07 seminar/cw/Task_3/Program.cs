7cc9c8ee52b2e0da1ea7f4422c45954d7f74d878
Egor Elushov <egorelusov@gmail.com>
2021-10-17 04:28:26 +0300

    add cw 7

---
+++ 01 module/07 seminar/cw/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        public static void Print(int[] a)
+        {
+            foreach (var el in a)
+            {
+                Console.Write(el + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+                a[i] = i + 1;
+            for(int i = 0; i < 100; i++)
+            {
+                int i1 = r.Next(0, 100), i2 = r.Next(0, 100);
+                (a[i2], a[i1]) = (a[i1], a[i2]);
+            }
+            Print(a);
+            Array.Resize(ref a, 99);
+            int sum = 0;
+            for (int i = 0; i < 99; i++)
+                sum += a[i];
+            Console.WriteLine(5050 - sum);
+        }
+    }
+}
