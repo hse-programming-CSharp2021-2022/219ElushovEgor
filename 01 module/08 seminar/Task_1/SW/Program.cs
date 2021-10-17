5ea8920bdd709bd7c76b6cd7cca66bbe5369ca9f
Egor Elushov <egorelusov@gmail.com>
2021-10-17 04:32:07 +0300

    add 08 cw and hw

---
+++ 01 module/08 seminar/Task_1/SW/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SW
+{
+    internal class Program
+    {
+        private static void Transform(ref uint numb)
+        {
+            uint n = 0;
+            for (uint i = 0; numb != 0; i++)
+            {
+                if (numb % 10 == 0)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 1)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 2)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 3)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 4)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 5)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 6)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 7)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 8)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+                else if (numb % 10 == 9)
+                {
+                    n += 10 * i * (numb % 10);
+                }
+
+                numb /= 10;
+            }
+
+            numb = n;
+        }
+        
+        public static void Main(string[] args)
+        {
+            uint N;
+            N = uint.Parse(Console.ReadLine() ?? string.Empty);
+            Transform(ref N);
+            Console.WriteLine(N);
+        }
+    }
+}
