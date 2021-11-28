d4371e8a96156ab16e79adb0cb2f6749cecc4f51
Егор Елушов <egorelusov@gmail.com>
2021-11-28 17:28:03 +0300

    add hw5

--- 02 module/05 seminar/CW/Task_1/Program.cs
+++ 02 module/05 seminar/CW/Task_1/Program.cs
@@ -1,26 +1,55 @@
 ﻿using System;
 
-namespace Task_1
+namespace Task_2
 {
+
     abstract class Animal
     {
-        private string Name { get; set; }
-        private string Sound { get; set; }
-
-        public abstract void AnimalSound();
-        public abstract void AnimalInfo();
+        public string Type;
+        public Animal(string name, int age)
+        {
+            AnimalAge = age;
+            AnimalName = name;
+        }
+        public string AnimalName { get; }
+        public int AnimalAge { get; }
+        public abstract string AnimalSound();
+        public abstract string AnimalInfo();
     }
-
     class Dog : Animal
     {
-        public override void AnimalInfo()
+        public Dog(string name, int age, string type, bool istrained) : base(name, age)
         {
-            throw new NotImplementedException();
+            Type = type;
+            isTrained = istrained;
         }
-
-        public override void AnimalSound()
+        private string Breed { get; }
+        private bool isTrained { get; }
+        public override string AnimalInfo()
         {
-            throw new NotImplementedException();
+            return $"Имя Собаки: {AnimalName}\nВозраст Собаки: {AnimalAge}\nПорода Собаки: {Breed}\nЗвук Собаки: {AnimalSound()}";
+        }
+        public override string AnimalSound()
+        {
+            return "Woof";
+        }
+    }
+    class Cow : Animal
+    {
+        public Cow(string name, int age, string type, bool istrained) : base(name, age)
+        {
+            Type = type;
+            isTrained = istrained;
+        }
+        private string Breed { get; }
+        private bool isTrained { get; }
+        public override string AnimalInfo()
+        {
+            return $"Имя коровы: {AnimalName}\nВозраст коровы: {AnimalAge}\nЗвук коровы: {AnimalSound()}";
+        }
+        public override string AnimalSound()
+        {
+            return "Moo";
         }
     }
     class Program
---
+++ 02 module/05 seminar/HW/Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Task04Lib_Shape;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int n1 = random.Next(3, 5), n2 = random.Next(3, 5), n3 = random.Next(3, 5);
+            var arr = new Shape[n1 + n2 + n3];
+            for(var i = 0; i < n1; i++)
+            {
+                arr[i] = new Circle(random.Next(1, 10));
+            }
+            for (var i = n1; i < n2 + n1; i++)
+            {
+                arr[i] = new Cylinder(random.Next(1, 10), random.Next(1, 10));
+            }
+
+            for (var i = n2 + n1; i < n1 + n2 + n3; i++)
+            {
+                arr[i] = new Sphere(random.Next(1, 10));
+            }
+            foreach (var i in arr)
+            {
+                if (i is Sphere)
+                    Console.Write("Sphere: ");
+                else if (i is Circle)
+                    Console.Write("Circle: ");
+                else
+                    Console.Write("Cylinder: ");
+                Console.WriteLine(i.Area());
+            }
+        }
+    }
+}
