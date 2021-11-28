a2c9715a8b939814e31b7bccacf2d018923cecff
Егор Елушов <egorelusov@gmail.com>
2021-11-28 18:51:39 +0300

    Add HW9

---
+++ 02 module/09 seminar/CW/Task_1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1
+{
+    public partial class Form1 : Form
+    {
+        bool a = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Text.Length == 0 && this.Opacity > 0.99)
+                a = false;
+            if (a && label1.Text.Length != 0)
+                label1.Text = (label1.Text.Remove(label1.Text.Length - 1, 1));
+            else if (a)
+            {
+                this.Opacity += 0.1;
+                if (this.Opacity >= 0.9)
+                {
+                    label1.Text = "Кот уже ушел";
+                    timer1.Stop();
+                }
+            }
+            else
+                this.Opacity -= 0.1;
+            if (this.Opacity <= 0)
+            {
+                a = true;
+            }
+        }
+    }
+}
---
+++ 02 module/09 seminar/CW/Task_2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var item in list)
+                listBox1.Items.Add(item);
+        }
+        List<string> list = new List<string>() { "1", "2", "3", "4"};
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach(var item in list)
+                listBox1.Items.Add(item);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                MessageBox.Show("Список пуст или нет выделенного!");
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+    }
+}
