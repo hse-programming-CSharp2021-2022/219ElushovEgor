52c554cb608b18ca11988f713d2c9dd52298f3ed
Егор Елушов <egorelusov@gmail.com>
2021-11-28 15:44:16 +0300

    Add CW and HW2

---
+++ 02 module/02 seminar/HW/Task1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Task1
+{
+    class MyComplex
+    {
+        public double re, im;
+
+        public MyComplex(double xre, double xim)
+        {
+            re = xre;
+            im = xim;
+        }
+
+        public static MyComplex operator --(MyComplex mc) => new (mc.re - 1, mc.im - 1);
+
+        public static MyComplex operator +(MyComplex firstComplex, MyComplex secondComplex) =>
+            new (firstComplex.re + secondComplex.re, firstComplex.im + secondComplex.im);
+
+        public static MyComplex operator -(MyComplex firstComplex, MyComplex secondComplex) =>
+            new (firstComplex.re - secondComplex.re, firstComplex.im - secondComplex.im);
+
+        public static MyComplex operator *(MyComplex firstComplex, MyComplex secondComplex) =>
+            new (firstComplex.re * secondComplex.re - firstComplex.im * secondComplex.im,
+                firstComplex.re * secondComplex.im + firstComplex.im * secondComplex.re);
+
+        public static MyComplex operator /(MyComplex firstComplex, MyComplex secondComplex) =>
+            new ((firstComplex.re * secondComplex.re + firstComplex.im * secondComplex.im) / 
+                 (secondComplex.re * secondComplex.re + secondComplex.im * secondComplex.im),
+                (firstComplex.im * secondComplex.re - firstComplex.re * secondComplex.im)/
+                (secondComplex.re * secondComplex.re + secondComplex.im * secondComplex.im));
+
+        public double Mod()
+        {
+            return Math.Abs(re * re + im * im);
+        }
+
+        public static bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+
+        public static bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"real={re}, image={im}";
+        }
+    }
+
+    class Program
+    {
+        static void Display(MyComplex cs)
+        {
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            MyComplex c1 = new MyComplex(4, 3.3);
+            MyComplex c2 = new MyComplex(2, 8.3);
+            Console.WriteLine($"a + b = {c1 + c2}");
+            Console.WriteLine($"a - b = {c1 - c2}");
+            Console.WriteLine($"a * b = {c1 * c2}");
+            Console.WriteLine($"a + b = {c1 / c2}");
+
+        }
+    }
+}
---
+++ 02 module/02 seminar/HW/Task_5/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Task_5
+{
+    public class ConsolePlate
+    {
+        char _plateChar;
+        ConsoleColor _plateColor = ConsoleColor.White, _backgroundColor = ConsoleColor.Black;
+
+        public ConsolePlate()
+        {
+            _plateChar = 'A';
+        }
+
+        public ConsolePlate(char plateChar='A', ConsoleColor plateColor= ConsoleColor.White, 
+            ConsoleColor backgroundColor = ConsoleColor.Black)
+        {
+            this.PlateChar = plateChar;
+            this.PlateColor = plateColor;
+            this.BackgroundColor = backgroundColor;
+        }
+
+        public char PlateChar
+        {
+            set
+            {
+                if (value >= 'A' && value <= 'Z')
+                    _plateChar = value;
+                else
+                    _plateChar = 'A';
+            }
+            get { return _plateChar; }
+        }
+        public ConsoleColor PlateColor
+        {
+            set { _plateColor = value; }
+            get => _plateColor;
+        }
+
+        public ConsoleColor BackgroundColor
+        {
+            set
+            {
+                if (value == _plateColor)
+                    _backgroundColor = _plateColor + 1;
+                else
+                    _backgroundColor = value;
+            }
+            get => _backgroundColor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cp = new ConsolePlate();
+            ConsolePlate[] somePlates =
+            {        
+                new ConsolePlate('X', ConsoleColor.White, ConsoleColor.Red),
+                new ConsolePlate('O', ConsoleColor.White, ConsoleColor.Magenta)
+            };
+            int n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < n; j++)
+                {
+                    Console.BackgroundColor = somePlates[(i + j) % 2].BackgroundColor;
+                    Console.ForegroundColor = somePlates[(i + j) % 2].PlateColor;
+                    Console.Write(somePlates[(i + j) % 2].PlateChar);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
